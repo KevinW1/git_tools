819f8f031ff641c41464605c132f04e933299e6a
Kevin Whitfield <kevinw@virtualmatter.org>
2024-02-23 03:17:48 -0500

    Add some metadata and coloring

--- git_tools.py
+++ git_tools.py
@@ -2,30 +2,53 @@ import argparse
 import logging
 import subprocess
 import sys
+from typing import Optional
 
 
-class term_colors:
+class TermString:
     """
     Class for terminal colors
 
-    Usage:
-        {color}{your_text}{END}
-        Example: f"{GREEN}{Hello World}{END}"
+    This is to avoid adding 3rd party packages
+
+    Args:
+        string: String to print in the terminal
+        *formatting: formats (colors, bold, etc) to apply
+
     """
 
-    GREEN = "\033[92m"
-    BOLD = "\033[1m"
-    END = "\033[0m"
+    formats = {
+        "GREEN": "\033[92m",
+        "CYAN": "\033[96m",
+        "BOLD": "\033[1m",
+        "YELLOW": "\033[33m",
+        "RED": "\033[31m",
+        "END": "\033[0m",
+    }
+
+    def __init__(self, string: str, *formatting: tuple):
+        self._string = str(string)
+        self._formatting = formatting
+
+    def __repr__(self) -> str:
+        output = self._string
+        for fmt in self._formatting:
+            output = f"{self.formats[fmt]}{output}{self.formats['END']}"
+        return output
+
+    def __len__(self) -> int:
+        return len(self._string)
 
 
-def exe_cmd(cmd):
+def exe_cmd(cmd: list) -> str:
     """
     executes a command in the shell and returns the output
     """
+    logging.debug(f"executing: {cmd}")
     return subprocess.run(cmd, capture_output=True, text=True, shell=True).stdout
 
 
-def current_branch():
+def current_branch() -> str:
     """
     Returns the current branch
     """
@@ -33,29 +56,29 @@ def current_branch():
     return exe_cmd(cmd).splitlines()[0]
 
 
-def upstream_tree():
+def latest_commit_hash(branch: str) -> str:
     """
-    Constructs a simple tree based on each branches upstream branch.
+    Returns the latest commit hash for the given branch
+    """
+    cmd = [f"git log --oneline -n 1 --format='%H' {branch}"]
+    return exe_cmd(cmd).splitlines()[0]
 
-    Yes this is a messy way to do it.  I'll fix it when it's worth the time.
+
+def latest_commit_title(branch: str) -> str:
+    """
+    Returns the latest commit title for the given branch
+    """
+    cmd = [f"git log --oneline -n 1 --format='%s' {branch}"]
+    return exe_cmd(cmd).splitlines()[0]
+
+
+def gather_upstreams() -> dict:
+    """
+    Finds upstream branch names for all local branches
 
     Returns:
-        Dict in the form "branch_name": "node" where node contains:
-            node.name - same as branch name in the dict
-            node.children - nodes that consider this node an upstream parent
-            node.active - is this the current branch? True/False
-            node.root - is this a root node? True/False
+        A dictionary of branch_name: upstream_branch
     """
-
-    class TreeNode:
-        def __init__(self, name):
-            self.name = name
-            self.children = []
-            self.active = False
-            self.root = False
-
-        def __repr__(self):
-            return self.name
 
     cmd = [
         "git for-each-ref --format='%(refname:short) %(upstream:short)' refs/heads",
@@ -63,7 +86,7 @@ def upstream_tree():
     result = exe_cmd(cmd)
 
     # Dictionary of branch_name: upstream_branch
-    tree_data = {}
+    branches = {}
     for line in result.splitlines():
         split = line.split(" ")
         branch = split[0]
@@ -74,16 +97,84 @@ def upstream_tree():
         if upstream == "" or upstream.startswith("origin/"):
             upstream = None
 
-        tree_data[branch] = upstream
+        branches[branch] = upstream
+    return branches
+
+
+def commit_count_difference(branch_a: str, branch_b: str) -> Optional[int]:
+    """
+    Returns the number of commits difference between two branches
+
+    Args:
+        branc_a, branc_b: string branch names to compare
+
+    Returns:
+        int difference, or None
+    """
+    if branch_a is None or branch_b is None:
+        return None
+
+    cmd = [
+        f"git rev-list {branch_a}..{branch_b} --count",
+    ]
+    result = exe_cmd(cmd).splitlines()
+
+    if result:
+        return int(result[0])
+    else:
+        return None
+
+
+def upstream_tree() -> dict:
+    """
+    Constructs a simple tree based on each branches upstream branch.
+
+    TODO: This is messy.  I'll fix it when it's worth the time.
+
+    Returns:
+        Dict in the form "branch_name": "node" where node contains:
+            node.name - same as branch name in the dict
+            node.children - nodes that consider this node an upstream parent
+            node.ahead - number of commits on this branch
+            node.behind - number of commits this branch is behind master
+            node.title - latest commit title
+            node.hash - latest commit hash
+            node.active - is this the current branch? True/False
+            node.root - is this a root node? True/False
+    """
+
+    class TreeNode:
+        def __init__(self, name: str):
+            self.name = name
+            self.children = []
+            self.ahead = 0
+            self.behind = 0
+            self.title = ""
+            self.hash = ""
+            self.active = False
+            self.root = False
+
+        def __repr__(self) -> str:
+            return self.name
+
+    branches = gather_upstreams()  # branch_name: upstream_branch
 
     # Dictionary of branch_name: tree_node
-    nodes = {branch: TreeNode(branch) for branch in tree_data}
+    nodes = {branch: TreeNode(branch) for branch in branches}
     active_branch = current_branch()
-    for branch, upstream in tree_data.items():
+    for branch, upstream in branches.items():
         node = nodes[branch]
+
+        # Set node metadata
+        node.behind = commit_count_difference(branch, upstream)
+        node.ahead = commit_count_difference(upstream, branch)
+        node.hash = latest_commit_hash(branch)
+        node.title = latest_commit_title(branch)
+
         if branch == active_branch:
             node.active = True
-        if upstream is not None:
+
+        if upstream is not None and upstream in nodes.keys():
             parent = nodes[upstream]
             parent.children.append(node)
         else:
@@ -97,9 +188,9 @@ def upstream_tree():
     return nodes
 
 
-def print_branch_tree():
+def branch_tree_string() -> str:
     """
-    Prints a simple tree based on branch upstream value.  SImilar to linux tree command.
+    Creates a simple tree based on branch upstream value.
 
     branch_no_upstream
     master
@@ -108,35 +199,88 @@ def print_branch_tree():
         │   └─ test_two
         └─ test_zoo
 
+    Additional information printed:
+        - number of commits this branch is behind master
+        - number of commits on this branch
+        - latest commit title
+        - latest commit hash
+
+    Returns:
+        String to be printed in terminal
     """
 
-    def print_tree(node, prefix="", is_first=False, is_last=True):
+    table = []
+
+    def fill_table(node, prefix="", is_first=False, is_last=True):
         # Color coding for active branch
         if node.active:
-            name = f"{term_colors.BOLD}{term_colors.GREEN}{node.name}{term_colors.END}"
-        else:
-            name = node.name
+            node.name = TermString(node.name, "BOLD", "GREEN")
 
         # Pipe printing logic
-        if is_first:
-            print(name)
+        pipe = "" if is_first else prefix + ("└─ " if is_last else "├─ ")
+        next_prefix = prefix + "    " if is_last else "│   "
+
+        # commit count coloring
+        if node.behind is None:
+            node.behind = ""
         else:
-            print(prefix + ("└─ " if is_last else "├─ ") + name)
-        prefix += "    " if is_last else "│   "
+            node.behind = TermString(-node.behind, "RED") if -node.behind < 0 else " 0"
+
+        if node.ahead is None:
+            node.ahead = ""
+        else:
+            node.ahead = (
+                TermString(f"+{node.ahead}", "GREEN") if node.ahead > 0 else " 0"
+            )
+
+        table.append(
+            [
+                pipe,
+                node.name,
+                node.behind,
+                node.ahead,
+                node.title[:50],
+                TermString(node.hash, "YELLOW"),
+            ]
+        )
+
+        # Set up arguments and recursively call its self
         children_count = len(node.children)
         for i, child in enumerate(node.children):
             is_last = i == children_count - 1
-            print_tree(child, prefix, False, is_last)
+            fill_table(child, next_prefix, False, is_last)
 
-    # Gathers data
+    # Gather upstream tree data and sort by children length and then name
     nodes = upstream_tree()
     roots = [node for node in nodes.values() if node.root]
-    # Sort by children length and then name
     roots.sort(key=lambda x: (len(x.children), x.name))
 
-    # Print the tree
+    # Fill the table list with data
     for i, root in enumerate(roots):
-        print_tree(root, is_first=True)
+        fill_table(root, is_first=True)
+
+    # Find the maximum width of each column.  The len() + 2 is column padding
+    column_widths = [max(len(item) + 2 for item in column) for column in zip(*table)]
+
+    # Generate the output string
+    output = ""
+    for row_items in table:
+        content_length = 0
+        column_width = 0
+        for i in range(len(row_items)):
+            content_length = len(row_items[i])
+            # first two columns are conjoined (pipe and branch name)
+            if i == 1:
+                column_width = column_widths[0] + column_widths[1]
+                content_length += len(row_items[i - 1])
+            # remaining columns are normal
+            elif i > 1:
+                column_width = column_widths[i]
+            padding = min(column_width - content_length, column_width)
+            output += str(row_items[i]) + " " * padding
+        output += "\n"
+
+    return output
 
 
 def main(argv):
@@ -175,7 +319,7 @@ def main(argv):
 
     if args.subcommand == "branch":
         if args.branch_subcommand == "tree":
-            print_branch_tree()
+            print(branch_tree_string())
         elif args.branch_subcommand == "flow":
             print("TODO")
         else:
